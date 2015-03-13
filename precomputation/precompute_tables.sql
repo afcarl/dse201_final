c555b421c6b9a63ebbf287b95cbcbbf58f9cb5ed
Kevin Dyer <kkdyer8506@gmail.com>
2015-03-12 22:57:48 -0700

    added pcustomer

--- precomputation/precompute_tables.sql
+++ precomputation/precompute_tables.sql
@@ -17,3 +17,22 @@ FROM sales, users, states
 WHERE sales.uid = users.id AND
         users.state = states.id
 GROUP BY states.id, states.name;
+
+-- CUSTOMER
+DROP TABLE IF EXISTS pcustomer;
+
+CREATE TABLE pcustomer
+(
+  id serial NOT NULL,
+  customer_name text NOT NULL,
+  quantity_sold integer NOT NULL,
+  dollar_value integer NOT NULL,
+  CONSTRAINT pcustomer_pkey PRIMARY KEY (id),
+  CONSTRAINT pcustomer_name_key UNIQUE (customer_name)
+);
+
+INSERT INTO pcustomer
+SELECT u.id as id, u.name as customer_name, sum(s.quantity) as quantity_sold, sum(s.price) as dollar_value
+FROM sales s
+INNER JOIN users u on s.uid=u.id
+GROUP BY u.id, u.name
