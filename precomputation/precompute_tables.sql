dccbf940ed23a139566383bfa534855a298878e7
Conway Wong <conwaywong@users.noreply.github.com>
2015-03-13 21:11:32 -0700

    rolled up precomputes

--- precomputation/precompute_tables.sql
+++ precomputation/precompute_tables.sql
@@ -10,13 +10,14 @@ CREATE TABLE pstates
   CONSTRAINT pstates_pkey PRIMARY KEY (id),
   CONSTRAINT pstates_name_key UNIQUE (state_name)
 );
-
+/*
 INSERT INTO pstates
 SELECT states.id, states.name as state_name, sum(sales.quantity) as quantity_sold, sum(sales.price) as dollar_value
 FROM sales, users, states
 WHERE sales.uid = users.id AND
         users.state = states.id
 GROUP BY states.id, states.name;
+*/
 
 -- CUSTOMER
 DROP TABLE IF EXISTS pcustomer;
@@ -30,12 +31,34 @@ CREATE TABLE pcustomer
   CONSTRAINT pcustomer_pkey PRIMARY KEY (id),
   CONSTRAINT pcustomer_name_key UNIQUE (customer_name)
 );
-
+/*
 INSERT INTO pcustomer
 SELECT u.id as id, u.name as customer_name, sum(s.quantity) as quantity_sold, sum(s.price) as dollar_value
 FROM sales s
 INNER JOIN users u on s.uid=u.id
 GROUP BY u.id, u.name;
+*/
+
+-- CATEGORY
+DROP TABLE IF EXISTS pcategory;
+
+CREATE TABLE pcategory
+(
+  id serial NOT NULL,
+  category_name text NOT NULL,
+  quantity_sold integer NOT NULL,
+  dollar_value integer NOT NULL,
+  CONSTRAINT pcategory_pkey PRIMARY KEY (id),
+  CONSTRAINT pcategory_name_key UNIQUE (category_name)
+);
+/*
+INSERT INTO pcategory
+SELECT c.id as id, c.name as category_name, sum(s.quantity) as quantity_sold, sum(s.price) as dollar_value
+FROM sales s
+INNER JOIN products p on s.pid=p.id
+INNER JOIN categories c on p.cid = c.id
+GROUP BY c.id, c.name;
+*/
 
 -- CUSTOMER_PRODUCT
 DROP TABLE IF EXISTS pcustomer_product;
@@ -57,6 +80,12 @@ FROM sales s
 INNER JOIN users u on s.uid=u.id
 INNER JOIN products p on s.pid=p.id
 GROUP BY u.id, p.id, u.name, p.sku;
+
+-- roll pcustomer_product into pcustomer
+INSERT INTO pcustomer
+SELECT customer_id, customer_name, sum(quantity_sold) as quantity_sold, sum(dollar_value) as dollar_value
+FROM pcustomer_product
+GROUP BY customer_id, customer_name;
 
 -- CATEGORY_STATE
 DROP TABLE IF EXISTS pcategory_state;
@@ -80,3 +109,15 @@ INNER JOIN states st ON u.state = st.id
 INNER JOIN products p ON p.id = sl.pid
 INNER JOIN categories c ON c.id = p.cid
 GROUP BY c.id, c.name, st.id, st.name;
+
+-- roll pcategory_state into pcategory
+INSERT INTO pcategory
+SELECT category_id, category_name, sum(quantity_sold) as quantity_sold, sum(dollar_value) as dollar_value
+FROM pcategory_state
+GROUP BY category_id, category_name;
+
+-- roll pcategory_state into pstates
+INSERT INTO pstates
+SELECT state_id, state_name, sum(quantity_sold) as quantity_sold, sum(dollar_value) as dollar_value
+FROM pcategory_state
+GROUP BY state_id, state_name;
