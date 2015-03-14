a2fe9c6c7b8980ce32248f4133e19828810eced6
Conway Wong <conwaywong@users.noreply.github.com>
2015-03-14 13:04:56 -0700

    trigger change

--- precomputation/precompute_triggers.sql
+++ precomputation/precompute_triggers.sql
@@ -74,12 +74,28 @@ AFTER INSERT ON sales
 -- TRIGGER FOR precomputed table pcustomer_product
 CREATE OR REPLACE FUNCTION pcustomer_product_trigger_f() 
 	RETURNS TRIGGER AS $BODY$
+	DECLARE
+		m_customer_id	integer;
+		m_customer_name	text;
+		m_product_id	integer;
+		m_product_name	text;
     BEGIN
+    
+		-- Insert or update the summary row with the new values.
+		SELECT c.id, c.name INTO m_category_id, m_category_name
+		FROM categories c
+		INNER JOIN products p ON c.id=p.cid
+		WHERE p.id=NEW.pid;
+    
 		UPDATE 	pcustomer_product
 		SET 	quantity_sold = quantity_sold + NEW.quantity,
 				dollar_value = dollar_value + NEW.price
 		WHERE 	customer_id = NEW.uid AND
 				product_id = NEW.pid;
+		IF NOT FOUND THEN
+			INSERT INTO pcustomer_product(customer_id, customer_name, product_id, product_name, quantity_sold, dollar_value)
+			VALUES (m_customer_id, m_customer_name, m_product_id, m_product_name, NEW.quantity, NEW.price)
+		END IF;
 		RETURN NULL;
 	END;
 $BODY$ LANGUAGE plpgsql;
@@ -93,16 +109,25 @@ AFTER INSERT ON sales
 -- TRIGGER FOR precomputed table pcategory
 CREATE OR REPLACE FUNCTION pcategory_trigger_f() 
 	RETURNS TRIGGER AS $BODY$
-    BEGIN
+	DECLARE
+		m_category_id	integer;
+		m_category_name	text;
+	BEGIN
+		-- Insert or update the summary row with the new values.
+		SELECT c.id, c.name INTO m_category_id, m_category_name
+		FROM categories c
+		INNER JOIN products p ON c.id=p.cid
+		WHERE p.id=NEW.pid;
+		
 		UPDATE 	pcategory
 		SET 	quantity_sold = quantity_sold + NEW.quantity,
 				dollar_value = dollar_value + NEW.price
-		WHERE 	id = (
-			SELECT c.id
-			FROM categories c
-			INNER JOIN products p on c.id=p.cid
-			WHERE p.id=NEW.pid
-		);
+		WHERE id = m_category_id;
+		
+		IF NOT found THEN
+			INSERT INTO pcategory (id, category_name, quantity_sold, dollar_value)
+			VALUES (m_category_id, m_category_name, NEW.quantity, NEW.price);
+		END IF;
 		RETURN NULL;
 	END;
 $BODY$ LANGUAGE plpgsql;
