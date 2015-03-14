dda765a77d418f53f6dff33588377cc09af434e9
Kevin Dyer <kkdyer8506@gmail.com>
2015-03-14 00:41:24 -0700

    added trigger t_update_pstates

--- precomputation/precompute_triggers.sql
+++ precomputation/precompute_triggers.sql
@@ -1,3 +1,28 @@
+-- TRIGGER FOR precomputed table pstates
+DROP TRIGGER IF EXISTS t_update_pstates on sales;
+
+CREATE OR REPLACE FUNCTION update_pstates()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+
+	UPDATE pstates 
+	SET quantity_sold=quantity_sold + NEW.quantity, dollar_value= dollar_value + NEW.price
+	WHERE id= (
+		SELECT st.id
+		FROM users u
+		INNER JOIN states st on u.state=st.id
+		WHERE u.id=NEW.uid
+	);
+	
+	RETURN NULL;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER t_update_pstates AFTER INSERT OR UPDATE
+   ON sales FOR EACH ROW
+   EXECUTE PROCEDURE public.update_pstates();
+
 -- TRIGGER FOR precomputed table pcustomer
 CREATE OR REPLACE FUNCTION pcustomer_trigger_f() 
 	RETURNS TRIGGER AS $BODY$
