c87781248130ff12dbd73cdfdc503054e67ba658
Kevin Dyer <kkdyer8506@gmail.com>
2015-03-14 01:03:19 -0700

    added trigger t_update_pcategory_state

--- precomputation/precompute_triggers.sql
+++ precomputation/precompute_triggers.sql
@@ -1,3 +1,33 @@
+-- TRIGGER FOR precomputed table pcategory_state
+DROP TRIGGER IF EXISTS t_update_pcategory_state on sales;
+
+CREATE OR REPLACE FUNCTION update_pcategory_state()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+	
+	UPDATE pcategory_state 
+	SET quantity_sold=quantity_sold + NEW.quantity, dollar_value= dollar_value + NEW.price
+	WHERE category_id= (
+		SELECT c.id
+		FROM categories c
+		INNER JOIN products p ON c.id=p.cid
+		WHERE p.id=NEW.pid
+	) AND state_id= (
+		SELECT st.id
+		FROM users u
+		INNER JOIN states st on u.state=st.id
+		WHERE u.id=NEW.uid
+	);
+	
+	RETURN NULL;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER t_update_pcategory_state AFTER INSERT OR UPDATE
+   ON sales FOR EACH ROW
+   EXECUTE PROCEDURE public.update_pcategory_state();
+
 -- TRIGGER FOR precomputed table pstates
 DROP TRIGGER IF EXISTS t_update_pstates on sales;
 
